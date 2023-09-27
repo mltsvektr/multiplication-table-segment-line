cb1e69692bbe9e36195476dc12959e34127ce695
mltsvektr <mltsvekaterina@yandex.ru>
2023-09-27 21:15:50 +0300

    method multiplyNumbers rewrote with StringBuilder, method printResult corrected

--- src/main/java/ru/maltseva/multiplication_table_segment_line/service/MultiplicationTable.java
+++ src/main/java/ru/maltseva/multiplication_table_segment_line/service/MultiplicationTable.java
@@ -3,12 +3,22 @@ package ru.maltseva.multiplication_table_segment_line.service;
 public class MultiplicationTable {
     public static void multiplyNumbers() {
         int result;
+        StringBuilder resultString = new StringBuilder();
         for (int a = 1; a <= 10; a++) {
             for (int b = 2; b <= 5; b++) {
                 result = a * b;
-                ResultWriter.printResult(b, a, result);
+                String tempStr = a + "x" + b + "=" + result;
+                resultString.insert(resultString.length(), tempStr);
+                if (tempStr.length() == 5) {
+                    resultString.insert(resultString.length(), "   ");
+                } else if (tempStr.length() == 6) {
+                    resultString.insert(resultString.length(), "  ");
+                } else {
+                    resultString.insert(resultString.length(), " ");
+                }
             }
-            System.out.println();
+            resultString.insert(resultString.length(), "\n");
         }
+        ResultWriter.printResult(resultString.toString());
     }
 }
--- src/main/java/ru/maltseva/multiplication_table_segment_line/service/ResultWriter.java
+++ src/main/java/ru/maltseva/multiplication_table_segment_line/service/ResultWriter.java
@@ -1,16 +1,10 @@
 package ru.maltseva.multiplication_table_segment_line.service;
 
 public class ResultWriter {
-    public static void printResult(int a, int b, int result) {
-        String output = a + "x" + b + "=" + result;
-        if (output.length() == 5) {
-            System.out.print(output + "   ");
-        } else if (output.length() == 6) {
-            System.out.print(output + "  ");
-        } else {
-            System.out.print(output + " ");
-        }
+    public static void printResult(String str) {
+        System.out.println(str);
     }
+
     public static void printResult(double result) {
         System.out.printf("%.3f", result);
     }
